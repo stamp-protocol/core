5bd24aec6bd634611de198a2a6b114f32650de9a
Andrew Danger Lyon <andrew@killtheradio.net>
2021-01-12 23:03:53 -0800

    switching back to base64.

--- src/util/ser.rs
+++ src/util/ser.rs
@@ -53,9 +53,9 @@ pub(crate) mod human_bytes {
         where S: Serializer,
     {
         if serializer.is_human_readable() {
-            serializer.collect_str(&format!("0x{}", hex::encode(bytes.as_slice())))
-            //serializer.serialize_str(&base64::encode_config(bytes.as_ref(), base64::STANDARD))
-            //serializer.collect_str(&base64::display::Base64Display::with_config(bytes.as_ref(), base64::STANDARD))
+            //serializer.collect_str(&format!("0x{}", hex::encode(bytes.as_slice())))
+            serializer.serialize_str(&base64::encode_config(bytes.as_slice(), base64::URL_SAFE_NO_PAD))
+            //serializer.collect_str(&base64::display::Base64Display::with_config(bytes.as_ref(), base64::URL_SAFE_NO_PAD))
         } else {
             //serde_bytes::serialize(bytes, serializer)
             serializer.serialize_bytes(bytes.as_ref())
@@ -67,8 +67,8 @@ pub(crate) mod human_bytes {
     {
         if deserializer.is_human_readable() {
             let s = <String>::deserialize(deserializer)?;
-            hex::decode(&s[2..]).map_err(de::Error::custom)
-            //base64::decode_config(s, base64::STANDARD).map_err(de::Error::custom)
+            //hex::decode(&s[2..]).map_err(de::Error::custom)
+            base64::decode_config(s, base64::URL_SAFE_NO_PAD).map_err(de::Error::custom)
         } else {
             serde_bytes::deserialize(deserializer)
             //let slice = <Vec<u8>>::deserialize(deserializer)?;
@@ -86,8 +86,8 @@ pub(crate) mod human_binary_from_slice {
               T: AsRef<[u8]>,
     {
         if serializer.is_human_readable() {
-            serializer.collect_str(&format!("0x{}", hex::encode(bytes.as_ref())))
-            //serializer.serialize_str(&base64::encode_config(bytes.as_ref(), base64::STANDARD))
+            //serializer.collect_str(&format!("0x{}", hex::encode(bytes.as_ref())))
+            serializer.serialize_str(&base64::encode_config(bytes.as_ref(), base64::URL_SAFE_NO_PAD))
         } else {
             serializer.serialize_bytes(bytes.as_ref())
         }
@@ -99,8 +99,8 @@ pub(crate) mod human_binary_from_slice {
     {
         if deserializer.is_human_readable() {
             let s = <String>::deserialize(deserializer)?;
-            let vec = hex::decode(&s[2..]).map_err(de::Error::custom)?;
-            //let vec = base64::decode_config(s, base64::STANDARD).map_err(de::Error::custom)?;
+            //let vec = hex::decode(&s[2..]).map_err(de::Error::custom)?;
+            let vec = base64::decode_config(s, base64::URL_SAFE_NO_PAD).map_err(de::Error::custom)?;
             let val = T::try_from_slice(&vec[..]).map_err(|_| de::Error::custom(String::from("bad slice length")))?;
             Ok(val)
         } else {
