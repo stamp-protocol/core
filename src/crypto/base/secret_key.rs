a72da9f413d27664e6248916e6f53a63569a28cd
Andrew Danger Lyon <andrew@killtheradio.net>
2024-06-25 23:14:35 -0700

    secret key: allow sending in AAD with plaintext.

--- src/crypto/base/secret_key.rs
+++ src/crypto/base/secret_key.rs
@@ -2,11 +2,79 @@ use crate::{
     error::{Error, Result},
     util::ser::{Binary, BinarySecret, BinaryVec, SerdeBinary},
 };
-use chacha20poly1305::aead::{Aead, KeyInit};
+use chacha20poly1305::aead::{Aead, KeyInit, Payload};
 use rand::{CryptoRng, RngCore};
 use rasn::{AsnType, Decode, Encode};
 use serde_derive::{Deserialize, Serialize};
 use std::ops::Deref;
+
+/// A structure to help with the bundling of AAD (additional associated data) with our ciphertext.
+#[derive(Debug, Clone, getset::Getters)]
+#[getset(get = "pub")]
+pub struct DataWithAAD<'msg> {
+    data: &'msg [u8],
+    aad: Option<Vec<u8>>,
+}
+
+impl<'msg> DataWithAAD<'msg> {
+    /// Create a new data+AAD payload
+    pub fn new(data: &'msg [u8], aad: Vec<u8>) -> Self {
+        Self { data, aad: Some(aad) }
+    }
+}
+
+impl<'msg> From<&'msg Vec<u8>> for DataWithAAD<'msg> {
+    fn from(data: &'msg Vec<u8>) -> Self {
+        Self {
+            data: data.as_slice(),
+            aad: None,
+        }
+    }
+}
+
+impl<'msg> From<&'msg [u8]> for DataWithAAD<'msg> {
+    fn from(data: &'msg [u8]) -> Self {
+        Self { data: data, aad: None }
+    }
+}
+
+impl<'msg, const N: usize> From<&'msg [u8; N]> for DataWithAAD<'msg> {
+    fn from(data: &'msg [u8; N]) -> Self {
+        Self {
+            data: data.as_slice(),
+            aad: None,
+        }
+    }
+}
+
+/// A self-describing, encrypted object that can be opened with the right key.
+#[derive(
+    Debug, Clone, PartialEq, AsnType, Encode, Decode, Serialize, Deserialize, getset::Getters, getset::MutGetters, getset::Setters,
+)]
+#[getset(get = "pub", get_mut = "pub(crate)", set = "pub(crate)")]
+pub struct Sealed {
+    /// Our heroic nonce
+    #[rasn(tag(explicit(0)))]
+    nonce: SecretKeyNonce,
+    /// The ciphertext
+    #[rasn(tag(explicit(1)))]
+    ciphertext: BinaryVec,
+    /// Optional additional associated data to store with the ciphertext.
+    #[rasn(tag(explicit(2)))]
+    aad: Option<BinaryVec>,
+}
+
+impl Sealed {
+    fn new(nonce: SecretKeyNonce, ciphertext: Vec<u8>, aad: Option<Vec<u8>>) -> Self {
+        Self {
+            nonce,
+            ciphertext: BinaryVec::from(ciphertext),
+            aad: aad.map(|x| BinaryVec::from(x)),
+        }
+    }
+}
+
+impl SerdeBinary for Sealed {}
 
 /// A symmetric encryption key nonce
 #[derive(Debug, Clone, PartialEq, AsnType, Encode, Decode, Serialize, Deserialize)]
@@ -23,31 +91,6 @@ pub enum SecretKey {
     #[rasn(tag(explicit(0)))]
     XChaCha20Poly1305(BinarySecret<32>),
 }
-
-/// A self-describing, encrypted object that can be opened with the right key.
-#[derive(
-    Debug, Clone, PartialEq, AsnType, Encode, Decode, Serialize, Deserialize, getset::Getters, getset::MutGetters, getset::Setters,
-)]
-#[getset(get = "pub", get_mut = "pub(crate)", set = "pub(crate)")]
-pub struct Sealed {
-    /// Our heroic nonce
-    #[rasn(tag(explicit(0)))]
-    nonce: SecretKeyNonce,
-    /// The ciphertext
-    #[rasn(tag(explicit(1)))]
-    ciphertext: BinaryVec,
-}
-
-impl Sealed {
-    fn new(nonce: SecretKeyNonce, ciphertext: Vec<u8>) -> Self {
-        Self {
-            nonce,
-            ciphertext: BinaryVec::from(ciphertext),
-        }
-    }
-}
-
-impl SerdeBinary for Sealed {}
 
 impl SecretKey {
     /// Create a new xchacha20poly1305 key
@@ -85,7 +128,7 @@ impl SecretKey {
     }
 
     /// Encrypt a value with a secret key/nonce
-    pub fn seal<'a, R: RngCore + CryptoRng>(&'a self, rng: &mut R, data: &[u8]) -> Result<Sealed> {
+    pub fn seal<'a, 'msg, D: Into<DataWithAAD<'msg>>, R: RngCore + CryptoRng>(&'a self, rng: &mut R, data: D) -> Result<Sealed> {
         match self {
             SecretKey::XChaCha20Poly1305(ref key) => {
                 let nonce = self.gen_nonce(rng)?;
@@ -94,16 +137,22 @@ impl SecretKey {
                 };
                 let secret: &'a [u8; 32] = key.expose_secret();
                 let cipher = chacha20poly1305::XChaCha20Poly1305::new(secret.into());
+                let data_w_aad = data.into();
+                let DataWithAAD { data, aad } = data_w_aad;
+                let payload = match aad.as_ref() {
+                    Some(aad) => Payload { msg: data, aad },
+                    None => data.into(),
+                };
                 let enc = cipher
-                    .encrypt(chacha20poly1305::XNonce::from_slice(nonce_bin.as_slice()), data)
+                    .encrypt(chacha20poly1305::XNonce::from_slice(nonce_bin.as_slice()), payload)
                     .map_err(|_| Error::CryptoSealFailed)?;
-                Ok(Sealed::new(nonce, enc))
+                Ok(Sealed::new(nonce, enc, aad))
             }
         }
     }
 
     /// Encrypt a value with a secret key/nonce
-    pub fn seal_with_nonce<'a>(&'a self, nonce: SecretKeyNonce, data: &[u8]) -> Result<Sealed> {
+    pub fn seal_with_nonce<'a, 'msg, D: Into<DataWithAAD<'msg>>>(&'a self, nonce: SecretKeyNonce, data: D) -> Result<Sealed> {
         match self {
             SecretKey::XChaCha20Poly1305(ref key) => {
                 let nonce_bin = match nonce {
@@ -111,10 +160,16 @@ impl SecretKey {
                 };
                 let secret: &'a [u8; 32] = key.expose_secret();
                 let cipher = chacha20poly1305::XChaCha20Poly1305::new(secret.into());
+                let data_w_aad = data.into();
+                let DataWithAAD { data, aad } = data_w_aad;
+                let payload = match aad.as_ref() {
+                    Some(aad) => Payload { msg: data, aad },
+                    None => data.into(),
+                };
                 let enc = cipher
-                    .encrypt(chacha20poly1305::XNonce::from_slice(nonce_bin.as_slice()), data)
+                    .encrypt(chacha20poly1305::XNonce::from_slice(nonce_bin.as_slice()), payload)
                     .map_err(|_| Error::CryptoSealFailed)?;
-                Ok(Sealed::new(nonce, enc))
+                Ok(Sealed::new(nonce, enc, aad))
             }
         }
     }
@@ -124,8 +179,15 @@ impl SecretKey {
         match (self, sealed.nonce()) {
             (SecretKey::XChaCha20Poly1305(ref key), SecretKeyNonce::XChaCha20Poly1305(ref nonce)) => {
                 let cipher = chacha20poly1305::XChaCha20Poly1305::new(chacha20poly1305::Key::from_slice(key.expose_secret().as_slice()));
+                let ciphertext = match sealed.aad().as_ref() {
+                    Some(aad) => Payload {
+                        msg: sealed.ciphertext().deref().as_slice(),
+                        aad: aad.deref().as_slice(),
+                    },
+                    None => sealed.ciphertext().deref().as_slice().into(),
+                };
                 let dec = cipher
-                    .decrypt(chacha20poly1305::XNonce::from_slice(nonce.as_slice()), sealed.ciphertext().deref().as_slice())
+                    .decrypt(chacha20poly1305::XNonce::from_slice(nonce.as_slice()), ciphertext)
                     .map_err(|_| Error::CryptoOpenFailed)?;
                 Ok(dec)
             }
@@ -179,6 +241,7 @@ pub(crate) mod tests {
                 8, 175, 83, 132, 142, 229, 0, 29, 187, 23, 223, 152, 164, 120, 206, 13, 240, 105, 184, 47, 228, 239, 34, 85, 79, 242, 230,
                 150, 186, 203, 156, 26,
             ],
+            None,
         );
         let key = SecretKey::new_xchacha20poly1305_from_bytes([
             120, 111, 109, 233, 7, 27, 205, 94, 55, 95, 248, 113, 138, 246, 244, 109, 147, 168, 117, 163, 48, 193, 100, 103, 43, 205, 212,
@@ -207,5 +270,69 @@ pub(crate) mod tests {
                 66
             ]
         );
+    }
+
+    #[test]
+    fn secretkey_xchacha20poly1305_seal_with_aad() {
+        let mut rng = crate::util::test::rng();
+        let key = SecretKey::new_xchacha20poly1305(&mut rng).unwrap();
+        let data_w_aad = DataWithAAD::new(b"hello there", vec![1, 2, 3]);
+        let sealed = key.seal(&mut rng, data_w_aad).unwrap();
+
+        {
+            let opened = key.open(&sealed).unwrap();
+            assert_eq!(&opened, b"hello there");
+        }
+
+        {
+            let mut sealed = sealed.clone();
+            sealed.set_aad(None);
+            let opened = key.open(&sealed);
+            assert!(matches!(opened, Err(Error::CryptoOpenFailed)));
+        }
+
+        {
+            let mut sealed = sealed.clone();
+            sealed.set_aad(Some(vec![1, 2, 4].into()));
+            let opened = key.open(&sealed);
+            assert!(matches!(opened, Err(Error::CryptoOpenFailed)));
+
+            sealed.set_aad(Some(vec![1, 2, 3].into()));
+            let opened = key.open(&sealed).unwrap();
+            assert_eq!(&opened, b"hello there");
+        }
+    }
+
+    #[test]
+    fn secretkey_xchacha20poly1305_seal_with_aad_nonce() {
+        let mut rng = crate::util::test::rng();
+        let key = SecretKey::new_xchacha20poly1305(&mut rng).unwrap();
+        let nonce = key.gen_nonce(&mut rng).unwrap();
+
+        let data_w_aad = DataWithAAD::new(b"hello there", vec![1, 2, 3]);
+        let sealed = key.seal_with_nonce(nonce, data_w_aad).unwrap();
+
+        {
+            let opened = key.open(&sealed).unwrap();
+            assert_eq!(&opened, b"hello there");
+        }
+
+        {
+            let mut sealed = sealed.clone();
+            sealed.set_aad(None);
+            let opened = key.open(&sealed);
+            assert!(matches!(opened, Err(Error::CryptoOpenFailed)));
+        }
+
+        {
+            let mut sealed = sealed.clone();
+            sealed.set_aad(Some(vec![1, 2, 4].into()));
+            let opened = key.open(&sealed);
+            assert!(matches!(opened, Err(Error::CryptoOpenFailed)));
+
+            sealed.set_aad(Some(vec![1, 2, 3].into()));
+            let opened = key.open(&sealed).unwrap();
+            assert_eq!(&opened, b"hello there");
+        }
     }
 }
